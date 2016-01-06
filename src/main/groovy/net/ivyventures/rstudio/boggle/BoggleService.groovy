422f548e69bc635938a2e68376e85711b46e9548
Joshua Spiewak <jspiewak@gmail.com>
2016-01-06 15:09:39 -0500

    Add simple 3 letter stems and add back parallel

--- src/main/groovy/net/ivyventures/rstudio/boggle/BoggleService.groovy
+++ src/main/groovy/net/ivyventures/rstudio/boggle/BoggleService.groovy
@@ -8,16 +8,20 @@ import java.util.stream.Stream
 
 import static java.util.function.Function.identity
 import static java.util.stream.Collectors.toList
+import static java.util.stream.Collectors.toSet
 
 @Slf4j
 @Service
 class BoggleService {
     Set<String> dictionary
+    Set<String> stems
     IntRange range = 0..3
 
     @PostConstruct
     void loadDictionary() {
         dictionary = (new ClassPathResource('boggle.dict').inputStream.readLines()) as Set<String>
+        stems = dictionary.parallelStream().map { it -> it[0..<3] }.distinct().collect(toSet())
+        log.info("Loaded ${dictionary.size()} words with ${stems.size()} stems")
     }
 
     List<String> findWords(List<List<String>> rows) {
@@ -39,11 +43,14 @@ class BoggleService {
         letters[r][c] = ''
 
         List<Stream<String>> streams = []
+
+        if (prefix.length() == 3 && !(prefix in stems)) return Stream.empty()
+
         [-1..1, -1..1].combinations().each { List<Integer> it ->
             Integer row = r + it[0]
             Integer col = c + it[1]
 
-            if (prefix.length() < 9 && (it[0] || it[1]) && range.contains(row) && range.contains(col) && letters[row][col]) {
+            if (prefix.length() < 10 && (it[0] || it[1]) && range.contains(row) && range.contains(col) && letters[row][col]) {
                 streams += findWords(prefix + letters[row][col], row, col, letters)
             }
 
@@ -53,6 +60,6 @@ class BoggleService {
             streams += Stream.of(prefix)
         }
 
-        Stream.of(streams.toArray()).flatMap(identity())
+        Stream.of(streams.toArray()).parallel().flatMap(identity())
     }
 }
